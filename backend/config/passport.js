f43544ca809b137467615b9a2be393e0fa224826
tylercchase <tylercchase@gmail.com>
2021-04-02 12:37:23 -0800

    Update eslint guidlines

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,7 +1,5 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-const mongoose = require('mongoose')
 const User = require('../models/User')
-const passport = require('passport')
 
 module.exports = (passport) => {
   passport.use(new GoogleStrategy({
