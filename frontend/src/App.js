b9de8e9619a74a36ce346ea02a22d49ca5d1d6f8
Tyler Chase <tylercchase@gmail.com>
2021-04-29 17:11:57 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,22 +27,6 @@ class App extends React.Component {
       />
       <Router>
         <div>
-          {false && <nav >
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>}
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
           <Switch>
             <Route path="/channel/:id" component={Channel} />
             <Route path="/">
